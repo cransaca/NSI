37370d17355f3af4d078f5ad82ca33e23d151da6
Didier Cransac <didier.cransac@ac-versailles.fr>
2019-06-12 23:28:04 +0200

    programme v2

---
+++ Foule.py
@@ -0,0 +1,95 @@
+﻿### Importations ###
+### Paramètres ###
+### Fonctions ###
+
+def conversionPolaireCartesien(r,theta):
+    return [x,y]
+
+def convertCartesienPolaire:
+
+
+
+### Classes ###
+
+class Terrain():
+    """Terrain dans lequel la foule se déplace."""
+    def __init__(self):
+        self.largeur=900
+        self.longeur=900
+
+        listAcces=[ [complex(200,largeur),complex(200+width,largeur)],[complex(1,2),complex(1,2)],[complex(1,2),complex(1,2)],[complex(1,2),complex(1,2)]]
+
+        listObstaclesFixes
+        listVoyageurs
+
+
+
+    def getObstacle
+
+    def creerVoyageur
+
+    def creerObstacle
+
+    def avanceVoyageur
+
+    def bouclePrincipale
+        while continue:
+            for voyag in self.listVoyageur:
+                voyag.positionSuivante
+
+
+class Obstacle:
+    """Les obstacles sont des disques.
+        Ils peuvent etre fixes, ou non comme les voyageurs,
+        Les murs sont des disques de rayon infini...
+    """
+
+    def __init__():
+
+        coordCentre =[int, int]
+        rayon=int
+        #forme
+
+    def getPosition
+        """Renvoie coordonnées, rayon et forme."""
+
+
+class Voyageur(Obstacle):
+    def __init__():
+        #boss
+
+        #coordCentre =[int, int]
+        #rayon
+        #forme
+        self.destination
+        self.vitesse
+        #vitesseNorme= int # Const  #complexe
+        #vitesseAngle
+        #vitesseVecteur=[int, int]
+
+
+        
+    def positionsuivante
+        listColision
+
+    def colision(self, obstacle):
+        return True//False
+
+    def avancer
+
+    def ChangerDirection
+
+    def testFin
+
+
+
+
+
+
+
+
+
+
+
+
+
